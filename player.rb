8c0f2768b64a5e9a42422ccefa2019a7c39cf92a
vitodub <105670453+vitodub@users.noreply.github.com>
2022-08-09 19:49:42 +0500

    Update player.rb

--- player.rb
+++ player.rb
@@ -1,15 +1,15 @@
 class Player
   def initialize(name) 
     @name = name
-	@bank = 100
-	@cards = []
+    @bank = 100
+    @cards = []
   end
 
   def place_bet
-	@bank -= 10
+    @bank -= 10
   end
 
   def add_card
-  	@cards << 'card'
+    @cards << 'card'
   end
 end
