d866758c51f7690c9f70f0186b5ed11cf0aca9c6
vitodub <105670453+vitodub@users.noreply.github.com>
2022-08-09 19:45:54 +0500

    Update interface.rb

--- interface.rb
+++ interface.rb
@@ -1,2 +1,14 @@
 class Interface
+  def call
+    loop do
+      run_interface
+      break if answer == 'Нет'
+    end
+  end
+
+  def run_interface
+  	Player.New(puts 'Введите ваше имя')
+  	Player.New('Дилер')
+  end
+
 end
