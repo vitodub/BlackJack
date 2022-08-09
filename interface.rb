f3ffd9c31e1fc6ce855d99a7a4b390a28d265eda
vitodub <105670453+vitodub@users.noreply.github.com>
2022-08-09 20:21:23 +0500

    Update interface.rb

--- interface.rb
+++ interface.rb
@@ -2,13 +2,16 @@ class Interface
   def call
     loop do
       run_interface
-      break if answer == 'Нет'
+      puts 'Хотите ли вы сыграть еще раз?'
+      play_again = gets.chomp.downcase
+      break if play_again == 'нет'
     end
   end
 
   def run_interface
-  	Player.New(puts 'Введите ваше имя')
-  	Player.New('Дилер')
+    puts 'Введите ваше имя'
+  	Player.new(gets.chomp)
+  	Player.new('Дилер')
   end
 
 end
