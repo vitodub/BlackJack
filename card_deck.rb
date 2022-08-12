c07072bf3451ce4b7ee804b558f6d135c53e0de2
vitodub <105670453+vitodub@users.noreply.github.com>
2022-08-12 14:35:44 +0500

    Update card_deck.rb

--- card_deck.rb
+++ card_deck.rb
@@ -1,14 +1,21 @@
 class CardDeck
-  
-  VALUES = { '2': 2, '3': 3, '4': 4, '5': 5,
-               '6': 6, '7': 7, '8': 8, '9': 9,
-              '10': 10, 'Валет': 10, 'Дама': 10,
-          'Король': 10, 'Туз': 11 }
+ 
+  VALUES = { '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+            '10': 10, 'Валет': 10, 'Дама': 10, 'Король': 10, 'Туз': 11 }
   SUITS = ["\u2660", "\u2665" , "\u2663", "\u2666"]
 
+  attr_reader :deck
 
-  def self.get_random(n)
-    VALUES.to_a.sample(n).to_h.transform_keys { |key| (key.to_s + SUITS.sample).to_sym }
+  def initialize
+    @deck = {}
+    get_deck
+  end
+
+  def get_deck
+    SUITS.each do |suit|
+      cards = VALUES.transform_keys { |value| (value.to_s + suit).to_sym }
+      @deck.merge!(cards)
+    end
   end
 
 end
