753db31d92f43317315af23a69484c7fb35d8e37
vitodub <105670453+vitodub@users.noreply.github.com>
2022-08-16 19:24:47 +0500

    Update player.rb

--- player.rb
+++ player.rb
@@ -8,13 +8,13 @@ class Player
 
   def initialize(name) 
     @name = name
-	  @bank = 100
-	  @cards = {}
+    @bank = 100
+    @cards = {}
     @sum
   end
 
   def place_bet
-	  @bank -= 10
+    @bank -= 10
   end
 
   def get_cards(cards, number)
@@ -22,20 +22,19 @@ class Player
   end
 
   def check_sum
-    s = @cards.values.sum
-    if s > MAX_POINT && @cards.has_value?(ACE_VALUE)
-      (s - MIN_MAX_ACE_DIFF) < MAX_POINT ? @sum = s - MIN_MAX_ACE_DIFF : @sum = s
+    subtotal = @cards.values.sum
+    if subtotal > MAX_POINT && @cards.has_value?(ACE_VALUE)
+      (subtotal - MIN_MAX_ACE_DIFF) < MAX_POINT ? @sum = subtotal - MIN_MAX_ACE_DIFF : @sum = subtotal
     else
-      @sum = s
+      @sum = subtotal
     end
   end
 
   def get_money
-  	@bank += 20
+    @bank += 20
   end
 
   def draw
     @bank += 10
   end
-
 end
