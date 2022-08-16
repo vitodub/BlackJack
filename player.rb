3a29ba4f17256b883e41f5b33e73c49fab3d3b4a
vitodub <105670453+vitodub@users.noreply.github.com>
2022-08-16 18:44:53 +0500

    Update player.rb

--- player.rb
+++ player.rb
@@ -4,13 +4,13 @@ class Player
 
   def initialize(name) 
     @name = name
-	  @bank = 100
-	  @cards = {}
+    @bank = 100
+    @cards = {}
     @sum
   end
 
   def place_bet
-	  @bank -= 10
+    @bank -= 10
   end
 
   def get_cards(cards, number)
@@ -27,7 +27,7 @@ class Player
   end
 
   def get_money
-  	@bank += 20
+    @bank += 20
   end
 
   def draw
